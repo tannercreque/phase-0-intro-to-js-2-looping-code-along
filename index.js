69554c217c8dc550b6e6baffa1cde534a4cbb93b
Tanner Creque <tanner.creque@gmail.com>
2021-03-07 10:28:13 -0700

    Finish Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+}
+const newArr = [];
+function writeCard(name, event){
+  for (let i = 0; i < names.length; i++){
+
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return newArr;
+}
